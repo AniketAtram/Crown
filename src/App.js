934b0cb1b3c321a52873ebf78004633cf4d610ef
AniketAtram <anniketatram@gmail.com>
2022-08-31 20:48:54 +0530

    created basic structure of application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className='category-container'>
+          {/* {image} */}
+          <div className='categoriy-body-conatiner'>
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+          <div className='categoriy-body-conatiner'>
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+          <div className='categoriy-body-conatiner'>
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+          <div className='categoriy-body-conatiner'>
+            <h2>Mens</h2>
+            <p>Shop Now</p>
+          </div>
+          <div className='categoriy-body-conatiner'>
+            <h2>Womens</h2>
+            <p>Shop Now</p>
+          </div>
+      </div>  
     </div>
   );
 }
