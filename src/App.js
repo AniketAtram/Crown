1c4674d1bbe84582d4fe3ff3a5b75bc33db9e4dd
AniketAtram <anniketatram@gmail.com>
2022-08-31 20:52:16 +0530

    corrected a mistake

--- src/App.js
+++ src/App.js
@@ -5,23 +5,34 @@ function App() {
     <div className="categories-container">
       <div className='category-container'>
           {/* {image} */}
-          <div className='categoriy-body-conatiner'>
+          <div className='category-body-conatiner'>
             <h2>Hats</h2>
             <p>Shop Now</p>
           </div>
-          <div className='categoriy-body-conatiner'>
+      </div>
+      <div className='category-container'>
+          {/* {image} */}
+          <div className='category-body-conatiner'>
             <h2>Jackets</h2>
             <p>Shop Now</p>
           </div>
-          <div className='categoriy-body-conatiner'>
+      </div>
+      <div className='category-container'>
+          {/* {image} */}
+          <div className='category-body-conatiner'>
             <h2>Sneakers</h2>
             <p>Shop Now</p>
           </div>
-          <div className='categoriy-body-conatiner'>
+      </div>
+      <div className='category-container'>
+          {/* {image} */}
+          <div className='category-body-conatiner'>
             <h2>Mens</h2>
             <p>Shop Now</p>
           </div>
-          <div className='categoriy-body-conatiner'>
+      </div><div className='category-container'>
+          {/* {image} */}
+          <div className='category-body-conatiner'>
             <h2>Womens</h2>
             <p>Shop Now</p>
           </div>
