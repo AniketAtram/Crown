392e774fc2edf0b37511b138a3db77ed0e2307a6
AniketAtram <anniketatram@gmail.com>
2022-09-05 20:52:59 +0530

    added images to card

--- src/components/CategoriesComponent/CategoryCard/CategoryCard.component.jsx
+++ src/components/CategoriesComponent/CategoryCard/CategoryCard.component.jsx
@@ -7,7 +7,7 @@ function CategoryCard({ data }) {
             {
                 data.map((item) => (
                     <div className='category-container' key={item.id}>
-                        <div className='background-image' />
+                        <div className='background-image' style={{"backgroundImage":`url(${item.imageUrl})`}} />
                         <div className='category-body-container'>
                             <h2>{item.title}</h2>
                             <p>Shop Now</p>
