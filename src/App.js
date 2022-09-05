c1fb4b32bc9a5f3b14e5c21ddece9cec9e90b598
AniketAtram <anniketatram@gmail.com>
2022-09-05 20:06:25 +0530

    Created an object for each item and used map function to display

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import './App.css';
 
 function App() {
+
+  const data = [
+    {
+      id: 1,
+      title: "Hats"
+    },
+    {
+      id: 2,
+      title: "Jackets"
+    },
+    {
+      id: 3,
+      title: "Sneakers"
+    },
+    {
+      id: 4,
+      title: "Mens"
+    },
+    {
+      id: 5,
+      title: "Womens"
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className='category-container'>
-          {/* {image} */}
-          <div className='category-body-conatiner'>
-            <h2>Hats</h2>
-            <p>Shop Now</p>
+      {
+        data.map((item) => (
+          <div className='category-container' key={item.id}>
+            {/* {image} */}
+            <div className='category-body-conatiner'>
+              <h2>{item.title}</h2>
+              <p>Shop Now</p>
+            </div>
           </div>
-      </div>
-      <div className='category-container'>
-          {/* {image} */}
-          <div className='category-body-conatiner'>
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>
-      <div className='category-container'>
-          {/* {image} */}
-          <div className='category-body-conatiner'>
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>
-      <div className='category-container'>
-          {/* {image} */}
-          <div className='category-body-conatiner'>
-            <h2>Mens</h2>
-            <p>Shop Now</p>
-          </div>
-      </div><div className='category-container'>
-          {/* {image} */}
-          <div className='category-body-conatiner'>
-            <h2>Womens</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>  
+        ))
+      }
     </div>
   );
 }
